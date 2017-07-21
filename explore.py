b37336b53b491a9ef2f535218367b1ab9d042c86
Zsolt Kovacs <lord@dkc5462.corp.lego.com>
2017-07-21 14:22:26 +0300

    TItle construction

--- explore.py
+++ explore.py
@@ -1,9 +1,11 @@
 # Data handling
 import pandas as pd
+import re
 
 # Visualization
 import seaborn as sns
 import matplotlib.pyplot as plt
+
 
 
 # Import data
@@ -11,14 +13,17 @@ trainDF = pd.read_csv("input/train.csv")
 testDF = pd.read_csv("input/test.csv")
 combine = pd.concat([trainDF, testDF], axis=0).reset_index(drop=True)
 
+r = re.compile('(\w*\.)')
+titles = list(map(lambda x: r.search(x).group(1), combine['Name']))
+
+
+
 
 # Null checking
 nulls = combine.isnull().sum().sort_values(ascending=False)
-nulls = nulls[nulls > 0]
-print('Null checking: \n', nulls)
 
 
-# Correlations
-g = sns.heatmap(trainDF[['Survived', 'SibSp', 'Parch', 'Age', 'Fare']].corr(), annot=True, cmap='coolwarm')
 
+
+# print(m.search(name).group(1))
 
